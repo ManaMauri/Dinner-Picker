db00ff5443667dfa48e30174e64df95d348c2da9
ManaMauri <147702066+ManaMauri@users.noreply.github.com>
2023-10-24 14:22:54 +1300

    Google sheet

--- script.js
+++ script.js
@@ -2,25 +2,12 @@ let options = [];
 let currentRotation = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Initialize the wheel with empty options
-    initializeWheel(options);
-
-    // File input change event
-    const fileInput = document.getElementById('excelFileInput');
-    fileInput.addEventListener('change', (e) => {
-        const file = e.target.files[0];
-        if (file) {
-            readExcelFile(file).then((newOptions) => {
-                options = newOptions;
-                initializeWheel(options);
-            });
-        }
-    });
-
-    // Load file button click event
-    const loadFileButton = document.getElementById('loadFileButton');
-    loadFileButton.addEventListener('click', () => {
-        fileInput.click();
+    // Fetch options from Google Spreadsheet
+    fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRvIjlDiuwZaZV8yrbj4fR2YIypZNzGkaxQ_mUX580Pxi3zCcmy0kV7ID15fokd4KahyJIYwXa4qF_8/pub?gid=0&single=true&output=csv")
+    .then(response => response.text())
+    .then(data => {
+        options = data.split("\n").slice(1);  // Skip header
+        initializeWheel(options);
     });
 
     // Spin button click event
@@ -28,31 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
     spinButton.addEventListener('click', () => {
         spinWheel(options);
     });
-});
 
-async function readExcelFile(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const data = e.target.result;
-            const workbook = XLSX.read(data, {
-                type: 'binary'
-            });
-
-            const worksheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[worksheetName];
-            const newOptions = XLSX.utils.sheet_to_json(worksheet);
-
-            resolve(newOptions.map(option => option.Name));
-        };
-
-        reader.onerror = function(ex) {
-            reject(ex);
-        };
-
-        reader.readAsBinaryString(file);
+    // Close popup
+    const popupClose = document.getElementById('popup-close');
+    popupClose.addEventListener('click', () => {
+        document.getElementById('popup').classList.add('hidden');
     });
-}
+});
 
 function initializeWheel(options) {
     const canvas = document.getElementById('wheelCanvas');
@@ -81,7 +50,7 @@ function initializeWheel(options) {
         ctx.moveTo(canvasCenter, canvasCenter);
         ctx.arc(canvasCenter, canvasCenter, radius, startAngle, endAngle);
         ctx.lineTo(canvasCenter, canvasCenter);
-        ctx.fillStyle = (i % 2 === 0) ? '#ddd' : '#eee';
+        ctx.fillStyle = (i % 2 === 0) ? '#FFC0CB' : '#FF69B4';  // Shades of pink
         ctx.fill();
         ctx.stroke();
 
@@ -90,7 +59,7 @@ function initializeWheel(options) {
         const x = canvasCenter + (radius * 0.5 * Math.cos(angle));
         const y = canvasCenter + (radius * 0.5 * Math.sin(angle));
         ctx.fillStyle = '#000';
-        ctx.font = '18px Arial';
+        ctx.font = '18px Open Sans';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
         ctx.fillText(options[i], x, y);
@@ -120,6 +89,9 @@ function spinWheel(options) {
         // Calculate the winning option
         const winningSegment = Math.floor(finalRotation / anglePerSegment);
         const winningOption = options[numSegments - winningSegment - 1];
-        alert(`You got: ${winningOption}`);
+
+        // Show custom popup
+        document.getElementById('popup-message').innerText = `You got: ${winningOption}`;
+        document.getElementById('popup').classList.remove('hidden');
     }, { once: true });
 }
